aa673e24ca717f0f04788a7277dca18b68e4a219
marcelopenas <marcelopenas@outlook.com>
2026-04-15 11:16:03 -0300

    vercel bruh

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Auth0Provider } from "@auth0/auth0-react";
-import { Analytics } from "@vercel/analytics/next";
 
 createRoot(document.getElementById("root")).render(
-  <Analytics>
-    <Auth0Provider
-      domain={import.meta.env.AUTH0_DOMAIN}
-      clientId={import.meta.env.AUTH0_CLIENT_ID}
-      authorizationParams={{
-        audience: import.meta.env.AUTH0_AUDIENCE,
-        redirect_uri: window.location.origin,
-      }}
-    >
-      <App />
-    </Auth0Provider>
-    ,
-  </Analytics>,
+  <Auth0Provider
+    domain={import.meta.env.AUTH0_DOMAIN}
+    clientId={import.meta.env.AUTH0_CLIENT_ID}
+    authorizationParams={{
+      audience: import.meta.env.AUTH0_AUDIENCE,
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <App />
+  </Auth0Provider>,
 );
