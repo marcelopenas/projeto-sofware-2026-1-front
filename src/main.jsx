2ea4f9a117a2351243184826dd9bd3d619b55b2b
marcelopenas <marcelopenas@outlook.com>
2026-04-15 11:14:07 -0300

    add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,21 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { Auth0Provider } from '@auth0/auth0-react'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { Analytics } from "@vercel/analytics/next";
 
-
-createRoot(document.getElementById('root')).render(
-  <Auth0Provider
+createRoot(document.getElementById("root")).render(
+  <Analytics>
+    <Auth0Provider
       domain={import.meta.env.AUTH0_DOMAIN}
       clientId={import.meta.env.AUTH0_CLIENT_ID}
       authorizationParams={{
         audience: import.meta.env.AUTH0_AUDIENCE,
-        redirect_uri: window.location.origin
+        redirect_uri: window.location.origin,
       }}
     >
-    <App />
-  </Auth0Provider>,
-)
+      <App />
+    </Auth0Provider>
+    ,
+  </Analytics>,
+);
