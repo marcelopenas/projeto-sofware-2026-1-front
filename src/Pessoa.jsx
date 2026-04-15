092fb6dd377c0c21038ffeff78f70c3b48d3fd4f
marcelopenas <marcelopenas@outlook.com>
2026-04-15 11:25:29 -0300

    fix env issue with vite

--- src/Pessoa.jsx
+++ src/Pessoa.jsx
@@ -27,7 +27,7 @@ export default function PessoasApp() {
         setToken(accessToken);
 
         const payload = JSON.parse(atob(accessToken.split(".")[1]));
-        const roles = payload[import.meta.env.ROLES_NAMESPACE] || [];
+        const roles = payload[import.meta.env.VITE_ROLES_NAMESPACE] || [];
         setIsAdmin(roles.includes("ADMIN"));
       } catch (e) {
         console.error("Erro ao buscar token:", e);
--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,10 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 createRoot(document.getElementById("root")).render(
   <Auth0Provider
-    domain={import.meta.env.AUTH0_DOMAIN}
-    clientId={import.meta.env.AUTH0_CLIENT_ID}
+    domain={import.meta.env.VITE_AUTH0_DOMAIN}
+    clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
     authorizationParams={{
-      audience: import.meta.env.AUTH0_AUDIENCE,
+      audience: import.meta.env.VITE_AUTH0_AUDIENCE,
       redirect_uri: window.location.origin,
     }}
   >
